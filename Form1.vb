1a71c93ac8c7740b3fbc6c4b4d7d691dc8cd2bc3
manish <manishsongara777@gmail.com>
2019-12-30 21:40:07 +0530

    renamed

--- Form1.vb
+++ Form1.vb
@@ -28,4 +28,9 @@ Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox1.Clear()
+        TextBox2.Clear()
+    End Sub
 End Class
